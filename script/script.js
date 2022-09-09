f6fb3fcbbfeaa306b2f5ff7cfabbbc873ae2e921
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-09-09 19:52:46 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,33 @@
+$(function(){
+
+
+
+})
+
+let arrow = document.querySelectorAll("span")
+let p = document.querySelector("p")
+
+console.log(arrow)
+
+
+for(let i = 0; i < arrow.length; i++){
+
+    arrow[i].addEventListener("click", function(event){
+   if(span.id === 3333){
+    p.classList = ".active"
+}
+    console.log(event)
+   })
+   
+
+
+};
+
+
+
+
+
+
+
+
+
