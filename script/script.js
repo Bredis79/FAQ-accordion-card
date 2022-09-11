a1f199bc71a5139acfc57d7dfdeec14ad97644bc
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-09-11 03:49:36 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,28 +1,50 @@
 $(function(){
 
+$("h2").on("mouseenter", function(event){
+    
+    $(".secondImg").animate({
+        marginLeft: "-517px"
+    }, 100)
 
+$("h2").on("mouseout", function(event){
+    $(".secondImg").animate({
+        marginLeft: "-480px"
+    },100)
+})    
+   
+})
 
 })
 
 let arrow = document.querySelectorAll("span")
-let p = document.querySelector("p")
+let p = document.querySelectorAll("p")
+let h2 = document.querySelectorAll("h2")
 
-console.log(arrow)
+
+for(let i = 0; i < arrow.length; i++){  //loop for arrow
+    for(let j = 0; j < p.length; j++){ //loop for paragraph
+        for(let k = 0; k < h2.length; k++){ //loop for h2
+    
+    h2[i].addEventListener("click", function(event){
+        
+        if(arrow[i].id === p[j].id) {
+            p[j].classList.toggle("active")
+        } else {
+             p[j].classList.remove("active")
+        }
+
+        if(arrow[i].id === h2[k].id){
+            h2[k].classList.toggle("bold")
+            arrow[i].classList.toggle("arrow")
+        } else {
+            h2[k].classList.remove("bold")
+        }
 
 
-for(let i = 0; i < arrow.length; i++){
-
-    arrow[i].addEventListener("click", function(event){
-   if(span.id === 3333){
-    p.classList = ".active"
+    })
+  }
+ }
 }
-    console.log(event)
-   })
-   
-
-
-};
-
 
 
 
