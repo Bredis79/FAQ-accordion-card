878cb7440c9e1dbe22cbc101970ea27084e0b1ca
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-09-11 16:36:16 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -31,6 +31,7 @@ for(let i = 0; i < arrow.length; i++){  //loop for arrow
             p[j].classList.toggle("active")
         } else {
              p[j].classList.remove("active")
+             arrow[i].classList.remove("arrow")
         }
 
         if(arrow[i].id === h2[k].id){
@@ -38,7 +39,7 @@ for(let i = 0; i < arrow.length; i++){  //loop for arrow
             arrow[i].classList.toggle("arrow")
         } else {
             h2[k].classList.remove("bold")
-            arrow[i].classList.remove("arrow")
+            
         }
 
 
