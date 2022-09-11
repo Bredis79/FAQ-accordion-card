c984a738aad5b506a91f80d957f8bd7b6b45870e
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-09-11 16:41:39 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -38,7 +38,6 @@ for(let i = 0; i < arrow.length; i++){  //loop for arrow
             arrow[i].classList.toggle("arrow")
         } else {
             h2[k].classList.remove("bold")
-            arrow.classList.remove("arrow")
         }
 
 
